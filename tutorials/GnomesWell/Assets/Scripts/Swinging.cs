918c9a8535a32ba55ce58ad568a8641007e3fc92
mounkeygirl <43825449+mounkeygirl@users.noreply.github.com>
2019-10-30 13:01:58 -0500

    AssestsFolder

---
+++ tutorials/GnomesWell/Assets/Scripts/Swinging.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swinging : MonoBehaviour
+{
+    public float swingSensitivity = 100.0f;
+
+    //use fixed update isntead of update, in order to play
+    //better with the physics engine
+    private void FixedUpdate()
+    {
+        //If we have no ridigbody (anymore), remoce this component
+        if (GetComponent<Rigidbody2D>() == null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        //Get the tilt amount form the InputManager
+        float swing = InputManager.instance.sidewaysMotion;
+
+        //calculate a force to apply 
+        Vector2 force =
+            new Vector2(swing * swingSensitivity, 0);
+
+        //apply the force
+        GetComponent<Rigidbody2D>().AddForce(force);
+    }
+}
